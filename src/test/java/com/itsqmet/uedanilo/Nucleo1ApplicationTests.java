45c2a306f5d65f1c74c2cd296728735d78b61a10
EliAlarcon <EliAlarcon@users.noreply.github.com>
2024-09-15 19:44:10 -0500

    Pruebas inserción de datos

--- src/test/java/com/itsqmet/uedanilo/Nucleo1ApplicationTests.java
+++ src/test/java/com/itsqmet/uedanilo/Nucleo1ApplicationTests.java
@@ -1,13 +1,34 @@
 package com.itsqmet.uedanilo;
 
+//import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//import com.itsqmet.uedanilo.modelo.entidades.Profesor;
+//import com.itsqmet.uedanilo.servicio.IProfesorService;
+
+@SpringBootTest(classes = Nucleo1Application.class)
 class Nucleo1ApplicationTests {
+
+	@Autowired
+	//private IProfesorService servicio;
 
 	@Test
 	void contextLoads() {
+
+		/*Profesor nuevo = new Profesor();
+		nuevo.setNombreProfesor("Boris");
+		nuevo.setApellidoProfesor("Luna");
+		nuevo.setEmail("boris@gmail.com");
+		nuevo.setEspecialidad("Físico Matemático");
+		servicio.createProfesor(nuevo);
+
+		List<Profesor> resultado = servicio.getProfesores();
+		for(Profesor profesor: resultado){
+			System.out.println(profesor.getNombreProfesor()+" "+profesor.getApellidoProfesor());
+		}*/
 	}
 
 }
